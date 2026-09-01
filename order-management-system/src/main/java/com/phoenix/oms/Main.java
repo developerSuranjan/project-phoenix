26114a2409d50b8ea37e561a77af55ceb3759905
Suranjan Das <91688087+developerSuranjan@users.noreply.github.com>
2026-09-01 23:46:34 +0530

    refactor: organize java code into package structure

---
+++ order-management-system/src/main/java/com/phoenix/oms/Main.java
@@ -0,0 +1,8 @@
+package com.phoenix.oms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Order Management System!");
+        // You can add more code here to initialize and run your application
+    }
+}
